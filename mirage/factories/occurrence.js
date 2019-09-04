4ede62ab896bcf6a1fc42fe405b772fa52485ba5
Josh Paul <josh4044@gmail.com>
2019-09-03 22:40:04 -0700

    Add mirage serializer to include occurrences in the service route.

---
+++ mirage/factories/occurrence.js
@@ -0,0 +1,12 @@
+import { Factory, association } from 'ember-cli-mirage';
+
+export default Factory.extend({
+	// providers: association(),
+	// consumers: association(),
+	// service: association(),
+	// slots() {
+	// 	const max = 50
+	// 	const min = 9
+	// 	return Math.floor(Math.random() * (max - min) + min)
+	// }
+});
--- mirage/scenarios/default.js
+++ mirage/scenarios/default.js
@@ -1,9 +1,26 @@
-export default function(/* server */) {
+export default function(server) {
 
   /*
     Seed your development database using your factories.
     This data will not be loaded in your tests.
   */
 
-  // server.createList('post', 10);
+  // server.createList('service', 3);
+  // server.loadFixtures('services');
+  // server.createList('occurrence', 4, { service: services[0]})
+  const service1 = server.create('service', {
+  	title: "Flow Basics (Level 1)",
+  	duration: "75",
+  	description: "These Level 1 classes are perfect for those that are brand new to yoga, new to flow style yoga, or coming back from a hiatus or injury. Classes include both flow and static postures, and though they don’t move quickly, students should come prepared to MOVE!",
+  	images: [
+  		{
+        src: "http://r.ddmcdn.com/s_f/o_1/cx_462/cy_245/cw_1349/ch_1349/w_720/APL/uploads/2015/06/caturday-shutterstock_149320799.jpg"
+    	}
+    ]
+  })
+
+  const occurrence1 = server.create('occurrence', {
+  	service: service1,
+  	slots: 4
+  })
 }
